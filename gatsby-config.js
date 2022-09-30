1eebda5533ddd6bc2c3a10246599f69c2d5b5213
Loris Sadriu <loris.sadriu@sup-logistik.de>
2022-09-30 22:05:52 +0200

    Schreibfehler verbessert

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
             slug: `/loli`,
           },
           {
-            title: `Abouts`,
+            title: `About`,
             slug: `/about`,
           },
         ],
